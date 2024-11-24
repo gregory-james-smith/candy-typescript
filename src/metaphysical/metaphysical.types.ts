bb0219169872616369762569b36ce54c5e87b4cc
gregory-james-smith <13038259+gregory-james-smith@users.noreply.github.com>
2024-11-24 12:24:08 +0000

    Biome linting

--- src/metaphysical/metaphysical.types.ts
+++ src/metaphysical/metaphysical.types.ts
@@ -1,10 +1,16 @@
-// biome-ignore lint/suspicious/noConfusingVoidType: <explanation>
 export type Nothing = void | null | undefined;
 
-export type EmptyString = ""
-// biome-ignore lint/suspicious/noConfusingVoidType: <explanation>
+export type EmptyString = "";
 export type EmptyArray = void[];
-export type EmptyObject = { [key in string]: never }
-export type EmptySet = Set<void>
+export type EmptyObject = { [key in string]: never };
+export type EmptySet = Set<void>;
 export type EmptyMap = Map<void, void>;
-export type Empty = false | 0 | 0n | EmptyString | EmptyObject | EmptyArray | EmptySet | EmptyMap;
+export type Empty =
+	| false
+	| 0
+	| 0n
+	| EmptyString
+	| EmptyObject
+	| EmptyArray
+	| EmptySet
+	| EmptyMap;
