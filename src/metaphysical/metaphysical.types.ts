76c4cf33e25aa2b165356fef445355c51b767130
gregory-james-smith <13038259+gregory-james-smith@users.noreply.github.com>
2025-06-28 00:03:42 +0100

    Empty object

--- src/metaphysical/metaphysical.types.ts
+++ src/metaphysical/metaphysical.types.ts
@@ -2,7 +2,16 @@ export type Nothing = void | null | undefined;
 
 export type EmptyString = "";
 export type EmptyArray = void[];
-export type EmptyObject = { [key in string]: never };
+
+/**
+ * An empty object
+ * 
+ * The problem with `{ [key in string]: never }}` is that any union type
+ * `T | EmptyObject` is subject to a heuristic where accessing a property
+ * incorrectly infers the variable to be type `T`.
+ */
+export type EmptyObject = { [K in keyof any]?: never };
+
 export type EmptySet = Set<void>;
 export type EmptyMap = Map<void, void>;
 export type Empty =
